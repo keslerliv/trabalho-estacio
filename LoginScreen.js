8da7832b1eb0f973a21611ea64f4ccceb46bae3c
EversonF <128655303+EversonF@users.noreply.github.com>
2023-06-04 22:57:42 -0300

    Add files via upload

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,77 +1,103 @@
-import { View, Button, StyleSheet, Image, TextInput, TouchableOpacity, Platform, Text } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Button,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  Text,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 
-import Logo from '../assets/images/Logo.png';
+import Logo from "../assets/images/Logo.png";
+import BottomPopup from "../src/BottomPopup";
 
 export default function LoginScreen({ navigation }) {
-    return (
+  const [showPopup, setShowPopup] = useState(true);
+
+  const onClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
-            <Image style={styles.logo} source={Logo} />
+          <Image style={styles.logo} source={Logo} />
 
-            <TextInput style={styles.input} 
-            placeholder="Email"
-            />
-
-            <TextInput style={styles.input} 
-            placeholder="Senha"
-            />
-
-            <View style={styles.forgotContainer}>
-                <TouchableOpacity>
-                    <Text style={styles.forgotText}>Esqueceu sua senha?</Text>
-                </TouchableOpacity>
-            </View>
-
-            <TouchableOpacity style={styles.loginButton}>
-                <Text style={styles.loginText}>Acessar</Text>
-                onPress={() => navigation.navigate('HomeScreen')}
+          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput style={styles.input} placeholder="Senha" />
+          <View style={styles.forgotContainer}>
+            {showPopup && (
+              <BottomPopup
+                title="Esqueceu sua senha?"
+                onTouchOutside={onClosePopup}
+              />
+            )}
+            <TouchableOpacity onPress={onClosePopup}>
+              <Text style={styles.forgotText}>Esqueceu sua senha?</Text>
             </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={() => navigation.navigate("HomeScreen")}
+          >
+            <Text style={styles.loginText}>Acessar</Text>
+          </TouchableOpacity>
         </View>
-    );
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20
-    },
-    logo: {
-        marginTop: Platform.OS === 'android' ? '13%' : '20%',
-        width: 166,
-        height: 59,
-        marginBottom: 30
-    },
-    input: {
-        width: '100%',
-        backgroundColor: '#F4F3F3',
-        marginBottom: 20,
-        borderWidth: 1,
-        borderColor: '#E0E0E0',
-        padding: 20,
-        borderRadius: 5
-    },
-    forgotContainer: {
-        width: '100%',
-        alignItems: 'flex-end',
-    },
-    forgotText: {
-        color:'#000',
-    },
-    loginButton:{
-        marginTop: '5%',
-        backgroundColor: '#000000',
-        width: '100%',
-        height: '42',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 5
-    },
-    loginText:{
-        color: '#FFF',
-        fontSize: 17,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    backgroundColor: "#FFF",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  logo: {
+    marginTop: Platform.OS === "android" ? "13%" : "20%",
+    marginBottom: Platform.OS === "android" ? "13%" : "20%",
+  },
+  input: {
+    width: "90%",
+    height: 42,
+    backgroundColor: "#F4F3F3",
+    marginBottom: 20,
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#E0E0E0",
+  },
+  forgotContainer: {
+    width: "90%",
+    alignItems: "flex-end",
+  },
+  forgotText: {
+    color: "#000000",
+  },
+  loginButton: {
+    marginTop: "5%",
+    backgroundColor: "#000000",
+    width: "90%",
+    height: 42,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  loginText: {
+    color: "#FFF",
+    fontSize: 17,
+  },
 });
